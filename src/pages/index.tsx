30c5daa1bbaf8d0966e0c10ccd8079b674c7f265
Pratheek <pratheekbhat2008@gmail.com>
2022-05-15 16:39:13 +0530

    REMOVED: navbar from index file, looks clean now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,16 @@
-import { AppShell, Paper, Text, Navbar, Header } from '@mantine/core'
+import { AppShell } from '@mantine/core'
 import type { NextPage } from 'next'
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import { IconFiles, IconFileSearch } from '@tabler/icons';
+import Sidebar from '../components/Sidebar'
 
 const Home: NextPage = () => {
   return (
     <AppShell
       padding="md"
-      navbar={
-      <Navbar width={{ base: 50 }} height={600} p="xs">
-        <Navbar.Section>
-          <IconFiles />
-        </Navbar.Section>
-        <Navbar.Section>
-          <IconFileSearch />
-        </Navbar.Section>
-      </Navbar>
-      }
-      
-      styles={(theme) => ({
-        main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
-      })}
-    >
-
+      navbar={<Sidebar />}      
+      styles={(theme) => ({ main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] }, })}
+      >
+    
       Application goes here 
       
     </AppShell>
